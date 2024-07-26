b05d0ae3f652b95b622bcd3ee8b68290b7234e98
Dayan <136735481+doocharbex@users.noreply.github.com>
2024-07-26 14:03:33 +0430

    [ Change Structure Project ]
    The main base of the project was changed because we needed a more dynamic and better environment for students

--- megademy/megademy/asgi.py
+++ megademy/megademy/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for class_room project.
+ASGI config for megademy project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'class_room.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'megademy.settings')
 
 application = get_asgi_application()
