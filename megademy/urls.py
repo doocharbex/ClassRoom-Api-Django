0e0c0911f41985267d78f967dab97ba69975e599
Dayan <136735481+doocharbex@users.noreply.github.com>
2024-07-26 15:56:08 +0430

    [Link 'api_creator' System in Main System]

--- megademy/urls.py
+++ megademy/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls') )
+    path('api/', include('api_creator.urls') )
+    
 ]
